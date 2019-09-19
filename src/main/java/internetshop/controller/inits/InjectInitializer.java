e53f5bb5efb49f6d69cfdb6e5ba41622a807ddb1
Max <skaterweather41@gmail.com>
2019-09-19 13:06:45 +0200

    added log4j loger; all sout replaced with logger

--- src/main/java/internetshop/controller/inits/InjectInitializer.java
+++ src/main/java/internetshop/controller/inits/InjectInitializer.java
@@ -1,17 +1,19 @@
 package internetshop.controller.inits;
 
 import internetshop.annotation.Injector;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class InjectInitializer implements ServletContextListener {
+    private static final Logger logger = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
             Injector.injectDependencies();
-            System.out.println("All dependencies injected");
+            logger.info("All dependencies injected");
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
